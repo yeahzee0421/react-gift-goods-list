e9b10fede1f32ca8b0ee05e75b613f19b0fe89c9
yeahzee0421 <ydw08250421@gmail.com>
2024-07-11 22:47:21 +0900

    refactor: refactor fetchData function

--- src/api/fetchData.ts
+++ src/api/fetchData.ts
@@ -1,13 +1,6 @@
 import apiClient from '.';
 //Promise
-export const fetchData = async <T>(
-  apiEndpoint: string,
-  params?: object,
-): Promise<T | undefined> => {
-  try {
-    const res = await apiClient.get<T>(apiEndpoint, { params });
-    return res.data;
-  } catch (error) {
-    console.error(error);
-  }
+export const fetchData = async <T>(apiEndpoint: string, params?: object): Promise<T> => {
+  const res = await apiClient.get<T>(apiEndpoint, { params });
+  return res.data;
 };
