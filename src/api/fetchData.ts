d99cbf66243e4ba6056ddaa9d4a99bea292e1764
yeahzee0421 <ydw08250421@gmail.com>
2024-07-12 15:04:15 +0900

    feat: update fetchData to throw Error

--- src/api/fetchData.ts
+++ src/api/fetchData.ts
@@ -1,10 +1,12 @@
 import apiClient from '.';
-
 export const fetchData = async <T>(
   apiEndpoint: string,
   params?: object,
 ): Promise<{ data: T; ok: boolean }> => {
   const res = await apiClient.get<T>(apiEndpoint, { params });
   const ok = res.status === 200;
+  if (!ok) {
+    throw new Error('Error to fetch Data!');
+  }
   return { data: res.data, ok };
 };
