932044611ff328c7d7fc47d98b776ee12500b32f
yeahzee0421 <ydw08250421@gmail.com>
2024-07-14 20:13:08 +0900

    feat: add useInfiniteQuery and InterSection Observer to GoodsSection

--- src/components/features/Theme/ThemeGoodsSection/index.tsx
+++ src/components/features/Theme/ThemeGoodsSection/index.tsx
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled';
-import { useSuspenseQuery } from '@tanstack/react-query';
+import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 import { API_ENDPOINT } from '@/api/constants/apiPath';
 import { fetchData } from '@/api/fetchData';
@@ -7,27 +9,46 @@ import { DefaultGoodsItems } from '@/components/common/GoodsItem/Default';
 import { Container } from '@/components/common/layouts/Container';
 import { Grid } from '@/components/common/layouts/Grid';
 import { breakpoints } from '@/styles/variants';
-import type { GetGoodsDataResponse, GoodsData } from '@/types';
+import type { GoodsData } from '@/types';
+
+interface ProductResponse {
+  products: GoodsData[];
+  nextPageToken: string | null;
+  pageInfo: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+}
 
 type Props = {
   themeKey: string;
 };
 
-const fetchGoodsData = async (themeKey: string) => {
+const fetchGoodsData = async (themeKey: string, pageParam: string) => {
   const params = {
     maxResults: 20,
+    pageToken: pageParam,
   };
-  const { data } = await fetchData<GetGoodsDataResponse>(
-    API_ENDPOINT.THEME_PRODUCTS(themeKey),
-    params,
-  );
-  return data.products;
+  const { data } = await fetchData<ProductResponse>(API_ENDPOINT.THEME_PRODUCTS(themeKey), params);
+  console.log(data);
+  return data;
 };
+
 export const ThemeGoodsSection = ({ themeKey }: Props) => {
-  const { data } = useSuspenseQuery<GoodsData[]>({
-    queryKey: ['goodsData'],
-    queryFn: () => fetchGoodsData(themeKey),
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useSuspenseInfiniteQuery({
+    queryKey: ['goodsData', themeKey],
+    queryFn: ({ pageParam = '1' }) => fetchGoodsData(themeKey, pageParam),
+    initialPageParam: '1',
+    getNextPageParam: (lastPage: ProductResponse) => lastPage.nextPageToken || undefined,
   });
+
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, isFetchingNextPage, fetchNextPage]);
 
   return (
     <Wrapper>
@@ -39,16 +60,25 @@ export const ThemeGoodsSection = ({ themeKey }: Props) => {
           }}
           gap={16}
         >
-          {data.map(({ id, imageURL, name, price, brandInfo }) => (
-            <DefaultGoodsItems
-              key={id}
-              imageSrc={imageURL}
-              title={name}
-              amount={price.sellingPrice}
-              subtitle={brandInfo.name}
-            />
-          ))}
+          {data.pages.map((page) =>
+            page.products.map((product) => (
+              <DefaultGoodsItems
+                key={product.id}
+                imageSrc={product.imageURL}
+                title={product.name}
+                amount={product.price.sellingPrice}
+                subtitle={product.brandInfo.name}
+              />
+            )),
+          )}
         </Grid>
+        <div ref={ref}>
+          {isFetchingNextPage
+            ? '다음 페이지 로딩 중..'
+            : hasNextPage
+              ? '상품 로딩 중..'
+              : '보여드릴 상품이 더 이상 없습니다.'}
+        </div>
       </Container>
     </Wrapper>
   );
